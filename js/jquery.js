4918060eef648415629d0d787d53bb9e919206de
vagrant <vagrant@lighthouse-labs>
2014-01-11 04:38:51 +0000

    Add and delete list items working

--- js/jquery.js
+++ js/jquery.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
 	$('.btn.btn-default').click(function() {
 		event.preventDefault();
 		var value = $('#inputTask').val();
-		$('.list-group').prepend('<li class="list-group-item"><input type ="checkbox" name="task">'+" "+value+'</li>');
+		$('.list-group').prepend('<li class="list-group-item"><input class="done" type="checkbox" name="task">'+" "+value+" "+'<span class="glyphicon glyphicon-remove"></li>');
+		$('form')[0].reset();
 	});
+
+	$('ul').on("click", ".glyphicon", function(){
+		$(this).parent().remove();
+
+	})
+
+	// $('.list-group-item').checked(function() {
+	// 	$('li').css('text-decoration:', 'line-through');
 });
