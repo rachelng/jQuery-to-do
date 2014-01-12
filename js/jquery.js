dcda96656a446c28e551831eb85a266fd3f5074f
vagrant <vagrant@lighthouse-labs>
2014-01-12 01:43:15 +0000

    Added calendar and edit buttons. Can now drag & drop

--- js/jquery.js
+++ js/jquery.js
@@ -2,15 +2,26 @@ $(document).ready(function(){
 	$('.btn.btn-default').click(function() {
 		event.preventDefault();
 		var value = $('#inputTask').val();
-		$('.list-group').prepend('<li class="list-group-item"><input class="done" type="checkbox" name="task">'+" "+value+" "+'<span class="glyphicon glyphicon-remove"></li>');
+		$('.list-group').prepend('<li class="list-group-item"><input class="done" type="checkbox" name="task">'+" "+value+" "+'<span class="glyphicon glyphicon-remove"></span><span class="glyphicon glyphicon-pencil"></span><span class="glyphicon glyphicon-calendar"></span></li>');
 		$('form')[0].reset();
 	});
 
-	$('ul').on("click", ".glyphicon", function(){
+	$('ul').on("click", ".glyphicon-remove", function(){
 		$(this).parent().remove();
+	});
 
-	})
+	// $('ul').on("click", ".glyphicon-calendar", function() {
+	// 	$(this).datepicker({
+	// 		inline:false,
+ //            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],  
+ //        });
+	// });
 
 	// $('.list-group-item').checked(function() {
 	// 	$('li').css('text-decoration:', 'line-through');
+
+	$(function() {
+	    $( ".list-group" ).sortable();
+	    $( ".list-group" ).disableSelection();
+	});
 });
