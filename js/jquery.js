ee0b5717b83862df4c281228a50ae0ebe18ddbfe
vagrant <vagrant@lighthouse-labs>
2014-01-13 03:16:15 +0000

    Working on implementing datepicker and edit task

--- js/jquery.js
+++ js/jquery.js
@@ -2,18 +2,31 @@ $(document).ready(function(){
 	$('.btn.btn-default').click(function() {
 		event.preventDefault();
 		var value = $('#inputTask').val();
-		$('.list-group').prepend('<li class="list-group-item"><input class="done" type="checkbox" name="task">'+" "+value+" "+'<span class="glyphicon glyphicon-remove"></span><span class="glyphicon glyphicon-pencil"></span><span class="glyphicon glyphicon-calendar"></span></li>');
+		$('.list-group').prepend('<li class="list-group-item"><input class="done" type="checkbox" name="task">'+" "+value+" "+'<span class="glyphicon glyphicon-remove"></span><span class="glyphicon glyphicon-calendar"></span><span class="glyphicon glyphicon-pencil"></span></li>');
 		$('form')[0].reset();
 	});
 
 	$('ul').on("click", ".glyphicon-remove", function(){
-		$(this).parent().remove();
+		$(this).parent().fadeOut();
 	});
 
+	$("ul").on("click", ".glyphicon-pencil", function(){
+  		$("#edit").show().val(
+  		$(this).text()).focus();
+	});
+
+	$("#edit").focusout(function(){
+		$(this).hide();  
+		$(this).siblings(".display").show().text(
+			$(this).val());
+	});
+
+	// $('ul').on("click", ".glyphicon-calendar", function(){
+	// 	$(this).datepicker();
+	// });
+
 	// $('ul').on("click", ".glyphicon-calendar", function() {
-	// 	$(this).datepicker({
-	// 		inline:false,
- //            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],  
+	// 	$(this).datepicker({ 
  //        });
 	// });
 
